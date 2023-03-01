8f45c447adefb443b0203644eb5b714477ae29aa
Ian Jones <ianjones834@protonmail.com>
2023-03-01 14:27:28 -0500

    finished basic platformer

---
+++ src/game.js
@@ -0,0 +1,23 @@
+import Phaser from 'phaser';
+import Play from './scenes/play';
+
+const game = new Phaser.Game({
+  type: Phaser.AUTO,
+  backgroundColor: 'black',
+  scale: {
+    width: 800,
+    height: 600,
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 }
+    }
+  }
+});
+
+game.scene.add('play', Play);
+
+game.scene.start('play');
